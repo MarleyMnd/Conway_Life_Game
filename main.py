3dc1574f53d24f25f91a40296d7ce8b4bdfe631c
Marley <mrl.menard@gmail.com>
2024-01-27 18:37:01 +0100

    kill cell

--- main.py
+++ main.py
@@ -4,22 +4,22 @@ import time
 
 BACKGROUND_COLOR = (50, 50, 50)
 GRID_COLOR = (20, 20, 20)
-GOING_TO_DIE_COLOR = (255, 153, 153)
+GOING_TO_DIE_COLOR = (110, 110, 110)
 ALIVE_COLOR = (255, 255, 255)
 
 
-def update_state(screen, cells, size, playing=False):
+def update_state(screen, cells, size, nb_generation, playing=False):
     grid = np.zeros((cells.shape[0], cells.shape[1]))
 
     for row, column in np.ndindex(cells.shape):
-        nb_alive_neighbors = np.sum(cells[row-1:row+2, column-1:column+2]) - cells[row, column]
+        nb_alive_neighbors = np.sum(cells[row - 1:row + 2, column - 1:column + 2]) - cells[row, column]
 
         if cells[row, column] == 0:
             color = BACKGROUND_COLOR
         else:
             color = ALIVE_COLOR
 
-        if cells[row,column] == 1:
+        if cells[row, column] == 1:
             if nb_alive_neighbors < 2 or nb_alive_neighbors > 3:
                 if playing:
                     color = GOING_TO_DIE_COLOR
@@ -33,7 +33,11 @@ def update_state(screen, cells, size, playing=False):
                 if playing:
                     color = ALIVE_COLOR
 
-        pygame.draw.rect(screen, color, (column*size, row*size, size - 1, size - 1))
+        pygame.draw.rect(screen, color, (column * size, row * size, size - 1, size - 1))
+
+    font = pygame.font.Font(None, 36)
+    text = font.render(f"Generation: {int(nb_generation)}", True, (255, 255, 255))
+    screen.blit(text, (10, 10))
 
     return grid
 
@@ -43,10 +47,11 @@ def main():
     screen = pygame.display.set_mode((1000, 800))
 
     cells = np.zeros((80, 100))
+    nb_generations = 0
 
     screen.fill(GRID_COLOR)
 
-    update_state(screen, cells, 10)
+    update_state(screen, cells, 10, nb_generations)
 
     pygame.display.flip()
     pygame.display.update()
@@ -61,27 +66,28 @@ def main():
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     running = not running
-                    update_state(screen, cells, 10)
+                    update_state(screen, cells, 10, nb_generations)
                     pygame.display.update()
 
             if pygame.mouse.get_pressed()[0]:
                 position = pygame.mouse.get_pos()
                 cells[position[1] // 10, position[0] // 10] = 1
-                update_state(screen, cells, 10)
+                update_state(screen, cells, 10, nb_generations)
                 pygame.display.update()
             if pygame.mouse.get_pressed()[2]:
                 position = pygame.mouse.get_pos()
                 cells[position[1] // 10, position[0] // 10] = 0
-                update_state(screen, cells, 10)
+                update_state(screen, cells, 10, nb_generations)
                 pygame.display.update()
 
         screen.fill(GRID_COLOR)
 
         if running:
-            cells = update_state(screen, cells, 10, playing=True)
+            cells = update_state(screen, cells, 10, nb_generations, playing=True)
+            nb_generations += 0.5
             pygame.display.update()
 
-        time.sleep(0.001)
+        time.sleep(0.0001)
 
 
 if __name__ == "__main__":
